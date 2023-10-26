6159c85bd3ca44b0b65a4c201e7a87d5c246f47a
Tomiwa <bankolefemi09@gmail.com>
2023-10-26 21:40:08 +0100

    Fix router config

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,9 +11,9 @@ function Header() {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
           <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/about">About</Nav.Link>
-          <Nav.Link href="/contact">Contact Us</Nav.Link>
-          <Nav.Link href="/login">Login</Nav.Link>
+          <Nav.Link href="about">About</Nav.Link>
+          <Nav.Link href="contact">Contact Us</Nav.Link>
+          <Nav.Link href="login">Login</Nav.Link>
         </Nav>
       </Navbar.Collapse>
       
