ecee8657abf5e0834bc778db8131db7a83572b7b
Tomiwa <bankolefemi09@gmail.com>
2023-10-26 21:44:14 +0100

    Fix router config

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import About from './components/About';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Login from './components/Login';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
